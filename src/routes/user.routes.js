b2f9686d82ada79b6164d9743d1dd4851b8f3752
Ankit Rayamajhi <ankitrayamajhi518@gmail.com>
2025-01-12 21:40:31 +0545

    📦 NEW: Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import {Router} from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken,
+    changeCurrentUserPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile  
+     } from "../controllers/user.controller.js";
 //To upload file like avtar ,cover image
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -32,5 +43,14 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)//not needed here = verifyJWT
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentUserPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails )
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar )
+router.route("/cover-image").patch(verifyJWT, upload.single("cover-image"), updateUserCoverImage  )
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile )
+router.route("/history").get(verifyJWT,updateAccountDetails )
+
+
 
 export default router
